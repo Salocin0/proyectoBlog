00e4213f2f834e59ffc3020c20d095f89bc05790
Salocin0 <nicolasarieldurelli@gmail.com>
2024-10-24 19:45:06 -0300

    detalle blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
+import DetalleBlog from './pages/Home/DetalleBlog';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>} />
+      <Route path='/blogs/:id' element={<DetalleBlog/>} />
     </Routes>
     </BrowserRouter>
   )
